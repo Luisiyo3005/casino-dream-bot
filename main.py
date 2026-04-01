1cf771d086abf61a11ee6b466c2824aa03892c74
Luisiyo3005 <luaas3005@gmail.com>
2026-04-01 04:39:26 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
 
+from telegram.ext import CommandHandler
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("balance", balance))
+app.add_handler(CommandHandler("slot", slot))
+app.add_handler(CommandHandler("blackjack", blackjack))
+app.add_handler(CommandHandler("ranking", ranking))
